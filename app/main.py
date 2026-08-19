27bb33f6dac8b404c79e42a0b4370373d22a85e5
Тимофей Яровик <timofey2009yarovik@gmail.com>
2026-08-19 16:28:15 +0300

    api router

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
-from http.client import HTTPException
-from fastapi import FastAPI, Response, status, Depends
+from fastapi import FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from passlib.context import CryptContext
 from .database import SessionLocal
-from sqlalchemy.orm import Session
-from . import models,schemas,utils
 import os
-from typing import List
-
+from .routers import posts, users
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app = FastAPI()
@@ -36,92 +32,9 @@ while True:
         time.sleep(1)
 
 
-@app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
-    print(posts)
-    return {"data": posts}
+app.include_router(users.router)
+app.include_router(posts.router)
 
-
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
-
-
-@app.get("/posts/{id}",response_model=schemas.Post)
-def get_post(id: int,db = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id).update({"published": False})
-    return post
-
-@app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
-    post = cursor.fetchone()
-    return post
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_post(post: schemas.CreatePost, db: Session = Depends(get_db)):
-    new_post = models.Post(title=post.title, content=post.content)
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
-
-
-@app.post("posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: schemas.CreatePost,):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s), 
-                   RETURNING *""", (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
-    return new_post
-
-
-@app.put("/posts/{id}",response_model=List[schemas.Post])
-def update_post(id: int, post: schemas.PostBase, db: Session = Depends(get_db)):
-    updated_post = db.query(models.Post).filter(models.Post.id == id).first()
-    updated_post = models.Post(title=post.title, content=post.content, published=updated_post.published)
-    db.commit()
-    db.refresh(updated_post)
-    return updated_post
-
-
-@app.put("/posts/{id}")
-def update_post(id: int, post: schemas.CreatePost,):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""" ,
-                   (post.title, post.content, post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    return updated_post
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s """, (str(id)))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOutput)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
-
-@app.get("/users/{id}",response_model=schemas.UserOutput)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).fetchone()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
 
 
 
---
+++ app/routers/posts.py
@@ -0,0 +1,28 @@
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from .. import models, schemas
+from ..database import get_db
+
+router = APIRouter(
+    prefix="/posts",
+    tags=["Posts"]
+)
+
+
+@router.get("/", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
+
+
+@router.get("/{id}", response_model=schemas.Post)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {id} was not found"
+        )
+    return post
