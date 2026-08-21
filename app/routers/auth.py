e24550fe5f25015341b636b2462d95a9e25907bc
Тимофей Яровик <timofey2009yarovik@gmail.com>
2026-08-21 17:17:55 +0300

    clearing code

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,9 +8,11 @@ router = APIRouter(
                    )
 
 @router.post('/login')
-def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(),
+          db: Session = Depends(get_db)):
 
-    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+    user = db.query(models.User).filter(
+        models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(
